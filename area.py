d3d434678329b6c42504fa821be04ac794e1bf1d
Agamdeep Singh <65022540+CodeWithAgam@users.noreply.github.com>
2021-11-10 00:05:36 +0530

    Added all the codes which contain the calculators

---
+++ area.py
@@ -0,0 +1,59 @@
+# The below function claculates the Area of a shapes
+# Created by Agamdeep Singh / CodeWithAgam
+# Insta: agamdeep_21
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+
+from math import *
+
+def calculate_area(name):
+    name = name.lower()
+
+    # check for the conditions
+    if name == "rectangle":
+        lenght = float(input("Length of Rectangle: "))
+        breadth = float(input("Breadth of Rectangle: "))
+        
+        # calculate area of rectangle
+        rect_area = lenght * breadth
+        print(f"The area of Rectangle is {rect_area}")
+
+    elif name == "square":
+        side = float(input("Lenght of side of square: "))
+        
+        # calculate area of square
+        square_area = side * side
+        print(f"The area of Square is {square_area}")
+
+    elif name == "triangle":
+        heightTri = float(input("Triangle's height: "))
+        baseTri = float(input("Triangle's base length: "))
+        
+        # calculate area of triangle
+        triangle_area = 0.5 * baseTri * heightTri
+        print(f"The area of Triangle is {triangle_area}")
+
+    elif name == "circle":
+        radius = float(input("Circle's radius: "))
+
+        # calculate area of circle
+        circle_area = 3.14 * radius * radius
+        print(f"The area of Circle is {circle_area}")
+            
+    elif name == "parallelogram":
+        baseParallelogram = float(input("Parallelogram's base: "))
+        heightParallelogram = float(input("Parallelogram's height: "))
+
+        # calculate area of parallelogram
+        parallelogram_area = baseParallelogram * heightParallelogram
+        print(f"The area of Parallelogram is {parallelogram_area}")
+        
+    else:
+        # this message gets printed if a shape entered by the user dosen't matches to the above conditions/shapes
+        print("Calculator: Sorry! This shape is not available")
+
+if __name__ == "__main__" :
+    print("Calculate Area")
+    print("Calculator: Which shape's area you want to calculate?")
+    shape_name = input("User: ")
+    calculate_area(shape_name)
---
+++ volume.py
@@ -0,0 +1,46 @@
+# The below function claculates the Volume of a shapes
+# Created by Agamdeep Singh / CodeWithAgam
+# Insta: agamdeep_21
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+
+from math import *
+
+def calculate_volume(name):
+    name = name.lower()
+
+    # check for the conditions
+    if name == "cuboid" or name == "rectangle":
+        lenght = float(input("Length of Cuboid: "))
+        breadth = float(input("Breadth of Cuboid: "))
+        height = float(input("Height of Cuboid: "))
+        
+        # calculate volume of rectangle
+        cuboid_volume = lenght * breadth * height
+        print(f"The volume of rectangle is {cuboid_volume}")
+
+    elif name == "cube" or name == "square":
+        side = float(input("Lenght of side of Cube: "))
+        
+        # calculate volume of square
+        square_volume = side * side * side
+        print(f"The volume of square is {square_volume}")
+
+
+    elif name == "circle" or name == "cyclinder" or name == "right circular cyclinder" or name == "circular cyclinder":
+        radiusCyclinder = float(input("Cyclinder's radius: "))
+        heightCyclinder = float(input("Cyclinder's height: "))
+
+        # calculate volume of circle
+        cyclinder_volume = 2 * 3.14 * radiusCyclinder * heightCyclinder
+        print(f"The volume of circle is {cyclinder_volume}")
+        
+    else:
+        # this message gets printed if a shape entered by the user dosen't matches to the above conditions/shapes
+        print("Calculator: Sorry! This shape is not available")
+
+if __name__ == "__main__" :
+    print("Calculate Volume")
+    print("Calculator: Which shape's volume you want to calculate?")
+    shape_name = input("User: ")
+    calculate_volume(shape_name)
