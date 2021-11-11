d60f2612e75e194269aaeb8f9f480de54c20b743
Agamdeep Singh <65022540+CodeWithAgam@users.noreply.github.com>
2021-11-12 00:34:00 +0530

    Added Curved Surface Area

---
+++ curved-sa.py
@@ -0,0 +1,17 @@
+# The below function claculates the Curved Surface Area of a Right Circular Cyclinder
+# Created by Agamdeep Singh / CodeWithAgam
+# Insta: agamdeep_21
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+
+from math import *
+
+print("Calculate Curved Surface Area of Right Circular Cyclinder")
+
+# Ask for the inputs
+radius = float(input("Cyclinder's radius: "))
+height = float(input("Cyclinder's height: "))
+
+# Calculate the Curved Surface Area and print it in the terminal
+cyclinder_csa = 2 * 3.14 * radius * height
+print(f"The Curved SA of your Cyclinder is {cyclinder_csa}")
