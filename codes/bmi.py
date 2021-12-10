05729b55c349029f2507b3c71e9ebc591373a3c1
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-11 01:06:24 +0530

    Updated the code

--- codes/bmi.py
+++ codes/bmi.py
@@ -15,9 +15,21 @@ def calculate_BMI():
     # The formula is, weight(kg) / height^2(m^2)
     BMI = int(weight / height**2)
 
-    # Print the output
-    print(f"Your Body Mass Index is {BMI}kg/m2")
+    # Check for the conditions and tell which category their BMI is.
+    if BMI < 18.5:
+        print(f"Your BMI is {BMI}, you are underweight!")
 
+    elif BMI < 25:
+        print(f"Your BMI is {BMI}, you have a normal weight!")
+
+    elif BMI < 30:
+        print(f"Your BMI is {BMI}, you are slightly overweight!")
+
+    elif BMI < 35:
+        print(f"Your BMI is {BMI}, you are obese!")
+        
+    else:
+        print(f"Your BMI is {BMI}, you are clinically obese!")
 
 print("Calculate the BMI or Body Mass Index")
 calculate_BMI()
