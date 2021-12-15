7146dff43622db50e8b76c43392c9063a7c8e675
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-15 10:01:27 +0530

    Updated the code

--- codes/surface-area.py
+++ codes/surface-area.py
@@ -17,14 +17,14 @@ def calculate_sa(name):
         
         # calculate Surface Area of rectangle
         cuboid_sa = 2 * ((lenght * breadth) + (breadth * height) + (height * lenght))
-        print(f"The Surface Area of rectangle is {cuboid_sa}")
+        print(f"The Surface Area of Cuboid is {cuboid_sa}")
 
     elif name == "cube" or name == "square":
         side = float(input("Lenght of side of Cube: "))
         
         # calculate Surface Area of square
         square_sa = 6 * (side * side)
-        print(f"The Surface Area of square is {square_sa}")
+        print(f"The Surface Area of Cube is {square_sa}")
 
 
     elif name == "cyclinder" or name == "right circular cyclinder" or name == "circular cyclinder":
@@ -33,7 +33,7 @@ def calculate_sa(name):
 
         # calculate Surface Area of circle
         cyclinder_sa = 2 * 3.14 * radiusCyclinder * (heightCyclinder + radiusCyclinder)
-        print(f"The Surface Area of circle is {cyclinder_sa}")
+        print(f"The Surface Area of Cyclinder is {cyclinder_sa}")
         
     else:
         # this message gets printed if a shape entered by the user dosen't matches to the above conditions/shapes
