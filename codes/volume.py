87c72b81863ffa4272027933cb2dedb3e325b5b3
Agamdeep Singh <agamdeep579@gmail.com>
2022-03-12 11:59:50 +0530

    Updated the code

--- codes/volume.py
+++ codes/volume.py
@@ -1,4 +1,4 @@
-# The below function claculates the Volume of a shapes
+# The below function calculates the Volume of a shapes
 # Created by Agamdeep Singh / CodeWithAgam
 # Youtube: CodeWithAgam
 # Github: CodeWithAgam
@@ -11,32 +11,33 @@ def calculate_volume(name):
 
     # check for the conditions
     if name == "cuboid" or name == "rectangle":
-        lenght = float(input("Length of Cuboid: "))
+        length = float(input("Length of Cuboid: "))
         breadth = float(input("Breadth of Cuboid: "))
         height = float(input("Height of Cuboid: "))
+        unit = input("Enter the Measuring Unit: ")
         
         # calculate volume of rectangle
-        cuboid_volume = lenght * breadth * height
-        print(f"The volume of rectangle is {cuboid_volume}")
+        cuboid_volume = length * breadth * height
+        print(f"The volume of rectangle is {cuboid_volume} {unit}cubed.")
 
     elif name == "cube" or name == "square":
-        side = float(input("Lenght of side of Cube: "))
+        side = float(input("Length of side of Cube: "))
         
         # calculate volume of square
         square_volume = side * side * side
         print(f"The volume of square is {square_volume}")
 
 
-    elif name == "circle" or name == "cyclinder" or name == "right circular cyclinder" or name == "circular cyclinder":
-        radiusCyclinder = float(input("Cyclinder's radius: "))
-        heightCyclinder = float(input("Cyclinder's height: "))
+    elif name == "circle" or name == "cylinder" or name == "right circular cylinder" or name == "circular cylinder":
+        radius_cylinder = float(input("Cylinder's radius: "))
+        height_cylinder = float(input("Cylinder's height: "))
 
         # calculate volume of circle
-        cyclinder_volume = 2 * 3.14 * radiusCyclinder * heightCyclinder
-        print(f"The volume of circle is {cyclinder_volume}")
+        cylinder_volume = 2 * 3.14 * radius_cylinder * height_cylinder
+        print(f"The volume of circle is {cylinder_volume}")
         
     else:
-        # this message gets printed if a shape entered by the user dosen't matches to the above conditions/shapes
+        # this message gets printed if a shape entered by the user doesn't match the above conditions/shapes
         print("Calculator: Sorry! This shape is not available")
 
 if __name__ == "__main__" :
